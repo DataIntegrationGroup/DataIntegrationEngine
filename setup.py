99e09ae28537309280e274cebb6bc7a472e5a324
jross <jake.ross@nmt.edu>
2024-05-17 16:20:21 -0600

    fixed setup

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name="nmuwd",
-    version="0.0.8",
+    version="0.0.9",
     author="Jake Ross",
     description="New Mexico Water Data Integration Engine",
     long_description=long_description,
@@ -37,7 +37,7 @@ setup(
             "weave = frontend.cli:cli",
         ],
     },
-    packages=["frontend", "backend"],
+    packages=["frontend"] + [f'backend.{p}' for p in find_packages('backend')],
     python_requires=">=3.6",
     include_package_data=True,
     # package_data={
