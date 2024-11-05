5f6cc3bfdf011341731ded5fee6d91695489babf
Jacob Brown <brown.jacob.alexander@gmail.com>
2024-11-04 17:03:12 -0700

    Clean ST2 waterlevel records - only return if not None

--- backend/connectors/st2/source.py
+++ backend/connectors/st2/source.py
@@ -87,6 +87,10 @@ class ST2WaterLevelSource(STWaterLevelSource):
 
     def _extract_parameter_results(self, records):
         return [r["observation"].result for r in records]
+    
+    def _clean_records(self, records: list) -> list:
+        rs = [r for r in records if r["observation"].result is not None]
+        return rs
 
     def get_records(self, site_record, *args, **kw):
         service = self.get_service()
