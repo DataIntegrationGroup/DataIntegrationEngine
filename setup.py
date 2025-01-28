50cb939bc4ed7f790efd9768846bdc8673fec00e
Jacob Brown <brown.jacob.alexander@gmail.com>
2025-01-28 11:57:33 -0700

    Update version to 0.2.1

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name="nmuwd",
-    version="0.2.0",
+    version="0.2.1",
     author="Jake Ross",
     description="New Mexico Water Data Integration Engine",
     long_description=long_description,
