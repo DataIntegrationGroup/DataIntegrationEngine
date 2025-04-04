49b5e9cf11462b88bd5ab8dbb041ef40561497c0
Jacob Brown <brown.jacob.alexander@gmail.com>
2025-04-04 11:34:26 -0600

    test NMBGMR

--- tests/source_tests/test_nmbgmr.py
+++ tests/source_tests/test_nmbgmr.py
@@ -1,4 +1,4 @@
-from backend.constants import WATERLEVELS
+from backend.constants import WATERLEVELS, CALCIUM
 from tests import BaseTestClass
 
 import pytest
@@ -10,32 +10,8 @@ class TestNMBGMRWaterlevels(BaseTestClass):
     units = "ft"
     agency = "nmbgmr_amp"
 
-    def test_health(self):
-        self._test_health()
+class TestNMBGMRAnalyte(BaseTestClass):
 
-    def test_summary(self):
-        self._test_summary()
-
-    @pytest.mark.skip(reason="Not implemented yet")
-    def test_timeseries_unified(self):
-        self._test_timeseries_unified()
-
-    @pytest.mark.skip(reason="Not implemented yet")
-    def test_timeseries_separated(self):
-        self._test_timeseries_separated()
-
-    @pytest.mark.skip(reason="Not implemented yet")
-    def test_date_range(self):
-        self._test_date_range()
-
-    @pytest.mark.skip(reason="Not implemented yet")
-    def test_wkt(self):
-        self._test_wkt()
-
-    @pytest.mark.skip(reason="Not implemented yet")
-    def test_county(self):
-        self._test_county()
-
-    @pytest.mark.skip(reason="Not implemented yet")
-    def test_huc(self):
-        self._test_huc()
+    parameter = CALCIUM
+    units = "mg/l"
+    agency = "nmbgmr_amp"
