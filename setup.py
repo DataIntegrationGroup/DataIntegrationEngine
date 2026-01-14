7ecc5c98fc18e5f2460b031321b952089debf912
jacob-a-brown <brown.jacob.alexander@gmail.com>
2026-01-14 12:00:42 -0700

    fix: bump version to 0.9.10

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name="nmuwd",
-    version="0.9.9",
+    version="0.9.10",
     author="Jake Ross",
     description="New Mexico Water Data Integration Engine",
     long_description=long_description,
