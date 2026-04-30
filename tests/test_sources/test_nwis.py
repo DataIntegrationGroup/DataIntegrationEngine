818330004ace4bc2747996a72771f629ae51285d
jacob-a-brown <brown.jacob.alexander@gmail.com>
2026-04-30 14:04:39 -0600

    test(usgs): use .env for api key for tests

--- tests/test_sources/test_nwis.py
+++ tests/test_sources/test_nwis.py
@@ -1,6 +1,20 @@
+import os
+from dotenv import load_dotenv
+import pytest
+
 from backend.constants import WATERLEVELS, FEET
 from tests.test_sources import BaseSourceTestClass
 
+@pytest.fixture(autouse=True)
+def setup():
+    # SETUP CODE -----------------------------------------------------------
+    load_dotenv()
+
+    # RUN TESTS ------------------------------------------------------------
+    yield
+
+    # TEARDOWN CODE ---------------------------------------------------------
+    os.environ["USGS_API_KEY"] = ""
 
 class TestNWISWaterlevels(BaseSourceTestClass):
 
